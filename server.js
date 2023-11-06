89f9396cd8b0e773cc84124f936da6e06bb8ece5
seanwsutter <seanwsutter@gmail.com>
2023-11-05 18:11:55 -0600

    fixed wrong starter files, added packagages, express server listening

--- server.js
+++ server.js
@@ -1,32 +1,26 @@
-const express = require('express');
-const path = require('path');
-const { logger } = require('./middleware/logger');
-const api = require('./routes/index.js');
-
-const PORT = process.env.port || 3001;
+const fs = require("fs");
+const express = require("express")
+const path = require("path")
 
 const app = express();
 
-// Import custom middleware, "logger"
-app.use(logger);
+const PORT = process.env.PORT || 3001;
 
-// Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
 
-app.use(express.static('public'));
+app.use(express.static("public"))
 
-// GET Route for homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
+app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "public/notes.html")));
 
-// GET Route for feedback page
-app.get('/feedback', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
-);
+app.get("/api/notes", (req, res) => res.sendFile(path.join(__dirname, "db/db.json")));
+
+// delete 
 
 app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT}`)
+  console.log(`Express server listening on port ${PORT}!`)
 );
+
+
+
+
